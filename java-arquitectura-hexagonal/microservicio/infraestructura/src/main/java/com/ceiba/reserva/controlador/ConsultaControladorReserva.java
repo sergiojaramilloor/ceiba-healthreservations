02c93d16f8f449e3ecf1c0312f123a1850848dff
Sergio Alberto Jaramillo Orozco - Ceiba Software <sergio.jaramillo@ceiba.com.co>
2021-04-09 09:15:21 -0500

    Se suben ajustes de CORS backend donde elimino el Cross Origin

--- java-arquitectura-hexagonal/microservicio/infraestructura/src/main/java/com/ceiba/reserva/controlador/ConsultaControladorReserva.java
+++ java-arquitectura-hexagonal/microservicio/infraestructura/src/main/java/com/ceiba/reserva/controlador/ConsultaControladorReserva.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/reservas")
 @Api(tags={"Controlador consulta reserva"})
